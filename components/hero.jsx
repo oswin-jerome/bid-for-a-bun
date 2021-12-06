ccbe4f875c715ccaf77f644ebeb74ef5677a785c
oswin-jerome <oswinjeromej@gmail.com>
2021-12-06 18:03:33 +0530

    v1

--- components/hero.jsx
+++ components/hero.jsx
@@ -11,10 +11,12 @@ const Hero = () => {
                     <h1 className=" text-center md:text-left text-3xl lg:text-5xl leading-[40px] lg:leading-[60px] mt-3 font-sans font-bold text-gray-600">
                         An initiative to receive <br/><span className="text-app-500">Heavens Manna </span><br/>early in the morning 
                     </h1>
-                    <a href="#groups">
+                    {/* <a href="#groups" className="flex"> */}
 
-                    <button  className= " bg-app-500 px-8 py-3 mt-10 text-lg rounded-lg shadow hover:shadow-lg hover:bg-app-600 text-white font-normal">Join us</button>
-                    </a>
+                    <button href="#groups"  className= " bg-app-500 px-8 py-3 mt-10 text-lg rounded-lg shadow hover:shadow-lg hover:bg-app-600 text-white font-normal">
+                        <a href="#groups">Join us</a>
+                    </button>
+                    {/* </a> */}
                 </div>
                 <div className="h-full flex-1 flex justify-center items-end">
                     <img src="assets/girl2.png" className="md:h-full h-auto" alt="" />
